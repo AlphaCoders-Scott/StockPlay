2a94b9edae789b5d1f78b294f0fada0ed1a9f961
vimarsh dubey <vimarshdb1@gmail.com>
2021-04-18 21:43:44 +0530

    Initial Commit

---
+++ Prices/src/test/java/com/StockPlay/Prices/PricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StockPlay.Prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
